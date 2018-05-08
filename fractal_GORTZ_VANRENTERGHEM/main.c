73c629487ba51dbbf6f7632b529daa1bdf1120a1
haiddoukable <39089261+haiddoukable@users.noreply.github.com>
2018-05-08 16:45:04 +0200

    Add files via upload

--- fractal_GORTZ_VANRENTERGHEM/main.c
+++ fractal_GORTZ_VANRENTERGHEM/main.c
@@ -1,11 +1,64 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <pthread.h>
 #include "fractal.h"
+
+int maxthread = 500;
+int showall =0;
+int nonfiles =0;
+int fcount = 0;
+pthread_t reader;
+pthread_t calc[];
+pthread_t comparateur;
+char *fract[][5];
 
 int main(int argc, char * argv[])
 {
-    /* TODO */
+	for(int i = 1; i<3;i++){
+		if(strcomp("-d",argv[i])){//récupération de showall
+			showall = 1;
+			i++;
+			nonfiles++;
+		}	
+		if(strcomp("--maxthreads",argv[i])){//récupération de maxthread	
+			maxthread = (argv[i+1]);
+			i++;
+			nonfiles+=2;
+		}
+	}
+	pthread_create(reader,&thread_reader,(void*) &argv[])//créateur du fichier 
 
-    return 0;
+	char* outfile = argv[argc-1]; //fichier output
+	
+}
+
+void *thread_reader(void args){
+	char* argv[] = (char *) args;
+	int argc = length(argv);
+	for(int i = argc-2;i > nonfiles;i--){
+		file_open(argv[i]);
+	}
+}
+
+
+//ouvre et écrit dans le tableau fract les specs des fractales dans le fichier filename
+//type de fichier name-w-h-cR-cI
+void file_open(filename)
+	FILE fp = fopen(filename,"r");
+	for(int i = 0; pas fini; i++){
+		fract[i][0] = fscanf(fp,'');
+	}
+	
+}
+
+void *thread_comparateur(){
+	while(fcount > 0){
+		sem_wait();
+		if(){
+			
+		}
+		sem_post();
+		
+	}
 }
