d635459318069d53a36c9596d2a3eeacf66fbcb3
LVanRent <louis.vanrenterghem@student.uclouvain.be>
2018-05-10 09:46:34 +0000

    Update main.c

--- fractal_GORTZ_VANRENTERGHEM/main.c
+++ fractal_GORTZ_VANRENTERGHEM/main.c
@@ -65,19 +65,33 @@ void *thread_reader(void args){
 
 /*ouvre et écrit dans le tableau fract les specs des fractales dans le fichier filename
 type de fichier name-w-h-cR-cI*/
-// /!\ commentaires #
-void file_open(string filename){
-	int scancount;
-	bufferList * new_fract = (bufferList *) malloc(sizeof(bufferList));
-	FILE *fp = fopen(filename,"r");
-	for(int i = 0; pas fini; i++){
-		scancount = fscanf(fp,"%s %d %d %f %f \n",new_fract->name,new_fract.w,new_fract.h,new_fract.cR,new_fract.cI);
-		if(scancount != 5) {
-		//erreur quelquepart
-		}
-		//à protéger
-		new_fract->next = lastread;
-		lastread = new_fract;
-	}
+void file_open(char * filename){
+	int scancount=5;
+	printf("fopen\n");
+	printf("filename1, %s\n",filename);
 	
+	FILE *fp = fopen(filename,"r");
+	if(fp == NULL) printf("failopen\n");
+	bufferList * new_fract ;
+	while(scancount != EOF){
+		new_fract = (bufferList *) malloc(sizeof(bufferList));
+		printf("malloc\n");
+		printf("fscanf\n");
+		scancount = fscanf(fp,"%s %d %d %lf %lf\n",&new_fract->name,&new_fract->w,&new_fract->h,&new_fract->cR,&new_fract->cI);
+		if(scancount != 5 && scancount != EOF) {
+			printf("%d %s \n", scancount,new_fract->name);
+			while(new_fract->name[0]=='#'){
+				while(fgetc(fp) !='\n'){}
+				scancount = fscanf(fp,"%s %d %d %lf %lf \n",&new_fract->name,&new_fract->w,&new_fract->h,&new_fract->cR,&new_fract->cI);
+			}
+		}
+		//producteur
+		if(scancount == 5){
+			printf("ajout de %s %d %d %lf %lf \n",&new_fract->name,&new_fract->w,&new_fract->h,&new_fract->cR,&new_fract->cI);
+			free(new_fract);
+		}
+	}
+	free(new_fract);
+		
 }
+
